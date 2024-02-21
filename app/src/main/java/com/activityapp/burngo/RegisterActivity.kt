2318993d210542e596715a8f544e380bf62673e7
TautvisP <59112653+TautvisP@users.noreply.github.com>
2024-02-21 18:07:13 +0200

    Added register form
    not completely finished, need login form to be done to connect them both

---
+++ app/src/main/java/com/activityapp/burngo/RegisterActivity.kt
@@ -0,0 +1,20 @@
+package com.activityapp.burngo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+//        TextView btn = findViewById(R.id.AlrdHaveAccountText)
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(New Intent(RegisterActivity.this,LoginActivity.class))
+//            }
+//        })
+    }
+}
