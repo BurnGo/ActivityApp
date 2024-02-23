ec7ef77c8b013e97b32b22d4b8826d3ea174f5af
TautvisP <59112653+TautvisP@users.noreply.github.com>
2024-02-23 19:21:30 +0200

    First iteration of progress bar
    Not tested, a lot of things need fixing

--- app/src/main/java/com/activityapp/burngo/RegisterActivity.kt
+++ app/src/main/java/com/activityapp/burngo/RegisterActivity.kt
@@ -3,18 +3,17 @@ package com.activityapp.burngo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.content.Intent
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-//        TextView btn = findViewById(R.id.AlrdHaveAccountText)
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(New Intent(RegisterActivity.this,LoginActivity.class))
-//            }
-//        })
+        val btn: TextView = findViewById(R.id.GoToStepsTEMP)
+        btn.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+        }
+
     }
 }
