8b4c023971f72a21484259b34802ef8ad784b8ac
TautvisP <59112653+TautvisP@users.noreply.github.com>
2024-03-25 19:24:13 +0200

    Added new pictures of tree's iterations

--- app/src/main/java/com/activityapp/burngo/TreeActivity.kt
+++ app/src/main/java/com/activityapp/burngo/TreeActivity.kt
@@ -13,10 +13,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class TreeActivity : AppCompatActivity() {
 
     private val treeIterations = listOf(
-        R.drawable.final_iteration,
-        R.drawable.final_iteration1,
-        R.drawable.final_iteration2,
-        R.drawable.final_iteration2
+        R.drawable.xfirst0,
+        R.drawable.xfirst1,
+        R.drawable.xfirst2,
+        R.drawable.xfirst3,
+        R.drawable.xfirst4,
+        R.drawable.xsecond1,
+        R.drawable.xsecond2,
+        R.drawable.xsecond3,
+        R.drawable.xsecond4,
+        R.drawable.xthird1,
+        R.drawable.xthird2,
+        R.drawable.xthird3,
+        R.drawable.xthird4
     )
 
     private var fertilizerCounter = 0
@@ -24,9 +33,9 @@ class TreeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tree)
         val treeImageView = findViewById<ImageView>(R.id.treeImageView)
-        var drawable = resources.getDrawable(R.drawable.final_test)
+        var drawable = resources.getDrawable(R.drawable.xfirst0)
         var bitmap = (drawable as BitmapDrawable).bitmap
-        var resizedBitmap = Bitmap.createScaledBitmap(bitmap, 400, 550, true)
+        var resizedBitmap = Bitmap.createScaledBitmap(bitmap, 300, 350, true)
         var currentIterationIndex = 0
 
         treeImageView.setImageBitmap(resizedBitmap)
@@ -39,16 +48,20 @@ class TreeActivity : AppCompatActivity() {
 
         val fertilizerImage = findViewById<ImageView>(R.id.fertilizer_image)
         fertilizerImage.setOnClickListener {
-            if (fertilizerCounter >= 2) {
+            if (fertilizerCounter >= 1) {
                 currentIterationIndex = (currentIterationIndex + 1)
 
-                // Change the picture of treeImageView to the next iteration
-                drawable = resources.getDrawable(treeIterations[currentIterationIndex])
-                bitmap = (drawable as BitmapDrawable).bitmap
-                resizedBitmap = Bitmap.createScaledBitmap(bitmap, 350, 450, true)
-                treeImageView.setImageBitmap(resizedBitmap)
+                if (currentIterationIndex < treeIterations.size) {
+                    // Change the picture of treeImageView to the next iteration
+                    drawable = resources.getDrawable(treeIterations[currentIterationIndex])
+                    bitmap = (drawable as BitmapDrawable).bitmap
+                    resizedBitmap = Bitmap.createScaledBitmap(bitmap, 350, 450, true)
+                    treeImageView.setImageBitmap(resizedBitmap)
 
-                fertilizerCounter = 0
+                    fertilizerCounter = 0
+                } else {
+                    Toast.makeText(this, "Congratulations! You've reached the final iteration.", Toast.LENGTH_SHORT).show()
+                }
 
             } else {
                 fertilizerCounter++
