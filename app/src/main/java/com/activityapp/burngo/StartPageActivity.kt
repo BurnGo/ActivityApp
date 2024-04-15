076d21d177e0a0974ca7b698be19bdc270a59eb2
Aruunce <arunas.skrudupis@gmail.com>
2024-04-15 21:29:56 +0300

    feat: notifications added

--- app/src/main/java/com/activityapp/burngo/StartPageActivity.kt
+++ app/src/main/java/com/activityapp/burngo/StartPageActivity.kt
@@ -1,19 +1,23 @@
 package com.activityapp.burngo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.activityapp.burngo.NotificationHelper
 
 class StartPageActivity : AppCompatActivity() {
 
     private lateinit var dbHelper: DBHelper
-
+    private lateinit var notificationHelper: NotificationHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_page)
 
         dbHelper = DBHelper(this)
+        notificationHelper = NotificationHelper()
 
         dbHelper.initialDataInsertion("Small car")
         dbHelper.initialDataInsertion("Medium car")
@@ -22,6 +26,16 @@ class StartPageActivity : AppCompatActivity() {
         signUpButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+
+            // Example: Show a notification when SignUpButton is clicked
+            val title = "Hello"
+            val message = "Thank you for trying our app!"
+            notificationHelper.showNotification(this, title, message)
+
+            val title2 = "Testing"
+            val message2 = "Testing notification after 20 seconds"
+            val delayMillis = 20L * 1L // 20 seconds in milliseconds
+            notificationHelper.scheduleNotification(this, title2, message2, delayMillis)
         }
 
     }
